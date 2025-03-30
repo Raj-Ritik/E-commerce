0d26c6b2cce930b4d0dca3a0dd36920ec99e09d1
Ritik Raj <ritikraj2308@gmail.com>
2025-03-30 22:14:42 +0530

    Topbar Design

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Topbar from "../Layout/Topbar";
+
+const Header = () => {
+  return (
+    <div>
+      {/*Top Bar*/}
+      <Topbar/>
+      {/*NavBar*/}
+      {/*cartDrawer*/}
+    </div>
+  );
+};
+
+export default Header;
