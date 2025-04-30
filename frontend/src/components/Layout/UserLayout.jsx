4faa65928e336e2243970cabd9f5916a15b57aaf
Ritik Raj <ritikraj2308@gmail.com>
2025-05-01 00:01:14 +0530

    Added Men and women collection section and New Arrival section

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../Common/Header";
 import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayout = () => {
   return (
@@ -7,6 +8,11 @@ const UserLayout = () => {
       {/*Header*/}
       <Header />
       {/*main Content*/}
+      <main>
+        {/*<Outlet /> component which will be replaced by whatever child route component is being rendered */}
+        {/* Outlet: A special component from React Router that serves as a placeholder for child routes */}
+        <Outlet />
+      </main>
       {/*Footer*/}
       <Footer />
     </>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Layout/Hero";
+import GenderCollectionSection from "../components/Products/GenderCollectionSection";
+import NewArrival from "../components/Products/NewArrival";
+const Home = () => {
+  return (
+    <div>
+      <Hero />;
+      <GenderCollectionSection />;
+      <NewArrival />;
+    </div>
+  );
+};
+
+export default Home;
