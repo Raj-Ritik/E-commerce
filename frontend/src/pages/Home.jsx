b125d8210715435b22d9840ddb88e508f3c25298
Ritik Raj <ritikraj2308@gmail.com>
2025-05-24 22:16:43 +0530

    Best Seller view and thumbnail feature added on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrival from "../components/Products/NewArrival";
+import ProductDetails from "../components/Products/ProductDetails";
 const Home = () => {
   return (
     <div>
-      <Hero />;
-      <GenderCollectionSection />;
-      <NewArrival />;
+      <Hero />
+      <GenderCollectionSection />
+      <NewArrival />
+      {/* best seller */}
+      <h2 className="text-3xl text-center font-bold mb-4">Best Seller</h2>
+      <ProductDetails />
     </div>
   );
 };
