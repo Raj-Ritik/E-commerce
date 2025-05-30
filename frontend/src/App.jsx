651260f31887eeba5592eb9aff56e46069eea620
Ritik Raj <ritikraj2308@gmail.com>
2025-05-30 17:55:13 +0530

    Added order-confirmation page with order detail and shipping date after payment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
 import Checkout from "./components/Cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 
 const App = () => {
   return (
@@ -17,12 +18,16 @@ const App = () => {
         <Route path="/" element={<UserLayout />}>
           {/*User Layout*/}
           <Route index element={<Home />} />
-          <Route path="login" element={<Login />}/>
-          <Route path="register" element={<Register />}/>
-          <Route path="profile" element={<Profile />}/>
-          <Route path="collections/:collection" element={<CollectionPage />}/>
-          <Route path="product/:id" element={<ProductDetails />}/>
-          <Route path="checkout" element={<Checkout />}/>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collections/:collection" element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
         </Route>
         <Route>{/*Admin Layout*/}</Route>
       </Routes>
