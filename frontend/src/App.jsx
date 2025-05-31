06a6a805589146d85bb481d4803ff3d29323b877
Ritik Raj <ritikraj2308@gmail.com>
2025-05-31 13:22:19 +0530

    Admin Dashboard homepage layout design done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
 
 const App = () => {
   return (
@@ -34,8 +35,9 @@ const App = () => {
           <Route path="order/:id" element={<OrderDetailsPage />} />
           <Route path="my-orders" element={<MyOrdersPage />} />
         </Route>
+        {/*Admin Layout*/}
         <Route path="/admin" element={<AdminLayout />}>
-          {/*Admin Layout*/}
+          <Route index element={<AdminHomePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
